d272a56e400b60519315868a7a77fdc9eb8e7898
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-07-01 23:11:23 +0100

    Adding interceptor to http Request +++ adding Headers

--- src/app/AuthInterceptor.service.ts
+++ src/app/AuthInterceptor.service.ts
@@ -1,9 +1,28 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    //restrict it here
+
+    // manip
+    //const modifRes = req.clone({url : 'blablabla'})
+    const modifRes = req.clone({headers:req.headers
+        .append('test','1111')
+        .append('Auth','SecretKeyDoNotPutItInTheCode'),
+      params:req.params.append('azerty','qwerty')})
+
     console.log('REquest On tje file')
-    return next.handle(req);
+   // do not forget to return the new one
+    return next.handle(modifRes).pipe(
+      tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response){
+          console.log('Response Has Arrived, Body data');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
