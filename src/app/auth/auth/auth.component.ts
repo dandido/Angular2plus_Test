2c6ab0b35f11b7c371ff38db6e0c990d1ac8ed0e
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-07-05 22:53:37 +0100

    Add Auth + fixing bug V1 : catching Error

---
+++ src/app/auth/auth/auth.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  caption1: string = "Sign Up";
+  caption2: string ="Switch to Login";
+  isUserLoggedIn = false;
+  isLoading = false;
+
+  errorAuth: string = "";
+
+  constructor(private authservice: AuthService) { }
+
+
+  ngOnInit(): void {
+  }
+
+  onSwitchMode(){
+    this.isUserLoggedIn = ! this.isUserLoggedIn;
+    this.caption2 = this.isUserLoggedIn ? "Switch to SignUp" : 'Switch to Login';
+
+
+  }
+
+  onSubmit(authRef: NgForm) {
+    if(!authRef.valid){
+      return;
+    }
+    this.errorAuth= "";
+    this.isLoading = true;
+
+    //console.log(authRef.value);
+    if(!this.isUserLoggedIn){
+      this.authservice.signUp(authRef.value.email,authRef.value.password).subscribe(response=> {
+        console.log(response);
+        this.isUserLoggedIn = true;
+        this.isLoading =false;
+      }, error => {
+        console.log(error);
+        this.errorAuth = error.error.error.message;
+        this.isLoading =false;
+      })
+    }else{
+      //.....
+    }
+    authRef.reset();
+
+      }
+}
